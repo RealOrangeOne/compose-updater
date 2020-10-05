efffe24d62eba68fda34d4bb63a9ee50b087a659
Jake Howard <git@theorangeone.net>
2020-10-05 14:55:24 +0100

    Allow multiple globs to be passed in, and resolved to individual files

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,28 @@
+use glob::glob;
+use std::path::PathBuf;
+use structopt::StructOpt;
+
+#[derive(StructOpt, Debug)]
+#[structopt()]
+struct Opt {
+    #[structopt(name = "files")]
+    files: Vec<String>,
+}
+
+fn get_files(files: &[String]) -> Option<Vec<PathBuf>> {
+    let mut all_files = Vec::new();
+    for file in files {
+        for path in glob(&file).ok()?.filter_map(Result::ok) {
+            if path.is_file() {
+                all_files.push(path);
+            }
+        }
+    }
+
+    Some(all_files)
+}
+
 fn main() {
-    println!("Hello, world!");
+    let opts = Opt::from_args();
+    println!("{:?}", get_files(&opts.files));
 }
